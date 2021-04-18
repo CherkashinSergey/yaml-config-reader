2b78df2fb64cbaaa5077d4154b709735861d0820
Sergey Cherkashin <4erkashin@list.ru>
2021-04-18 16:46:59 +0300

    Add output config

--- conf.c
+++ conf.c
@@ -1,32 +1,3 @@
-/*
- * Example libyaml parser.
- *
- * This is a basic example to demonstrate how to convert yaml to raw data
- * using the libyaml emitter API. Example yaml data to be parsed:
- *
- *    $ cat fruit.yaml
- *    ---
- *    fruit:
- *    - name: apple
- *      color: red
- *      count: 12
- *    - name: orange
- *      color: orange
- *      count: 3
- *    - name: bannana
- *      color: yellow
- *      count: 4
- *    - name: mango
- *      color: green
- *      count: 1
- *    ...
- *
- *    $ ./parse < fruit.yaml
- *    data[0]={name=apple, color=red, count=12}
- *    data[1]={name=orange, color=orange, count=3}
- *    data[2]={name=bannana, color=yellow, count=4}
- *    data[3]={name=mango, color=green, count=1}
- */
 #include <yaml.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -54,175 +25,190 @@ typedef struct pipe_config {
 } pipe_config;
 
 /* Parser states. */
-enum state_value {
+enum value_state {
     WAIT_DIRECTIVE,
     ACCEPT_KEY,
     ACCEPT_VALUE
 };
 
-enum parser_stage {
+enum pipe_config_state {
     START,
     ACCEPT_PIPELINE,
     FINISH_PIPELINE,
     ACCEPT_PIPELINE_NAME,
     ACCEPT_PIPELINE_TYPE,
     ACCEPT_TRANSFORM,
-    ACCEPT_TRANSFORM_WORKERS,
-    ACCEPT_TRANSFORM_BUFFER,
-    ACCEPT_TRANSFORM_BUFFER_SIZE,
-    ACCEPT_TRANSFORM_BUFFER_EVENTS,
-    FINISH_TRANSFORM_BUFFER,
-    FINISH_TRANSFORM,
+    ACCEPT_OUTPUT,
     STOP,
     ERROR
 };
 
+enum stage_config_state {
+    STAGE_ROOT,
+    WORKERS,
+    BUFFER,
+    BUFFER_SIZE,
+    BUFFER_EVENTS
+};
+
+union nested_config_state {
+    enum stage_config_state stage;
+};
+
 struct parser_state {
-    enum state_value state;
-    enum parser_stage stage;
+    enum value_state value_state;
+    enum pipe_config_state pipe_state;
+    union nested_config_state nested_state;
     int accepted;
     int error;
-    char *key;
-    char *value;
     pipe_config data;
 };
 
 int setPipeline(struct parser_state *s, yaml_event_t *event)
 {
     char *key = NULL;
-    if (s->state == ACCEPT_KEY){
+    if (s->value_state == ACCEPT_KEY){
         switch (event->type) {
         case YAML_SCALAR_EVENT:
             key = (char*)event->data.scalar.value;
             if (strcmp(key, "pipeline_name") == 0) {
-                s->stage = ACCEPT_PIPELINE_NAME;
-                s->state = ACCEPT_VALUE;
+                s->pipe_state = ACCEPT_PIPELINE_NAME;
+                s->value_state = ACCEPT_VALUE;
             } else if (strcmp(key, "pipeline_type") == 0) {
-                s->stage = ACCEPT_PIPELINE_TYPE;
-                s->state = ACCEPT_VALUE;
+                s->pipe_state = ACCEPT_PIPELINE_TYPE;
+                s->value_state = ACCEPT_VALUE;
             } else if (strcmp(key, "transform") == 0) {
-                s->stage = ACCEPT_TRANSFORM;
-                s->state = ACCEPT_KEY;
+                s->pipe_state = ACCEPT_TRANSFORM;
+                s->nested_state.stage = STAGE_ROOT;
+                s->value_state = ACCEPT_KEY;
+            } else if (strcmp(key, "output") == 0) {
+                s->pipe_state = ACCEPT_OUTPUT;
+                s->nested_state.stage = STAGE_ROOT;
+                s->value_state = ACCEPT_KEY;
             } else {
-                fprintf(stderr, "Unknown key: %s\n", s->key);
-                s->stage = ERROR;
+                fprintf(stderr, "Unknown key: %s\n", key);
+                s->pipe_state = ERROR;
             }
             break;
         case YAML_MAPPING_END_EVENT:
             /* Pileline options are finished. */
-            s->state = FINISH_PIPELINE;
+            s->value_state = FINISH_PIPELINE;
             s->accepted = 1;
             break;
         default:
             fprintf(stderr, "Unexpected event while getting key: %d\n",
                     event->type);
-            s->state = ERROR;
+            s->value_state = ERROR;
             break;
         }
     }
-    else if (s->state == ACCEPT_VALUE){
+    else if (s->value_state == ACCEPT_VALUE){
         if (event->type != YAML_SCALAR_EVENT) {
             fprintf(stderr, "Unexpected event while getting value: %d\n",
                     event->type);
-            s->state = ERROR;
+            s->value_state = ERROR;
         } else {
-            s->value = (char*)event->data.scalar.value;
-            switch (s->stage)
+            char *value = (char*)event->data.scalar.value;
+            switch (s->pipe_state)
             {
             case ACCEPT_PIPELINE_NAME:
-                s->data.name = strdup((char*)s->value);
+                s->data.name = strdup(value);
                 break;
             case ACCEPT_PIPELINE_TYPE:
-                s->data.type = strdup((char*)s->value);
+                s->data.type = strdup(value);
                 break;
             default:
                 break;
             }
-            s->state = ACCEPT_KEY;
+            s->value_state = ACCEPT_KEY;
         }
     }
 }
 
-int setTransform(struct parser_state *s, yaml_event_t *event)
+int setTransform(struct parser_state *s, yaml_event_t *event, stage_config *config)
 {
-    if (s->state == ACCEPT_KEY){
+    if (s->value_state == ACCEPT_KEY){
         char *key = NULL;
         switch (event->type) {
         case YAML_SCALAR_EVENT:
             key = (char*)event->data.scalar.value;
-            if (s->stage == ACCEPT_TRANSFORM && strcmp(key, "workers") == 0) {
-                s->stage = ACCEPT_TRANSFORM_WORKERS;
-                s->state = ACCEPT_VALUE;
-            } else if (s->stage == ACCEPT_TRANSFORM && strcmp(key, "buffer") == 0) {
-                s->stage = ACCEPT_TRANSFORM_BUFFER;
-            } else if (s->stage == ACCEPT_TRANSFORM_BUFFER && strcmp(key, "size") == 0) {
-                s->stage = ACCEPT_TRANSFORM_BUFFER_SIZE;
-                s->state = ACCEPT_VALUE;
-            } else if (s->stage == ACCEPT_TRANSFORM_BUFFER && strcmp(key, "events") == 0) {
-                s->stage = ACCEPT_TRANSFORM_BUFFER_EVENTS;
-                s->state = ACCEPT_VALUE;
+            if (s->nested_state.stage == STAGE_ROOT && strcmp(key, "workers") == 0) {
+                s->nested_state.stage = WORKERS;
+                s->value_state = ACCEPT_VALUE;
+            } else if (s->nested_state.stage == STAGE_ROOT && strcmp(key, "buffer") == 0) {
+                s->nested_state.stage = BUFFER;
+            } else if (s->nested_state.stage == BUFFER &&
+                       strcmp(key, "size") == 0) {
+                s->nested_state.stage = BUFFER_SIZE;
+                s->value_state = ACCEPT_VALUE;
+            } else if (s->nested_state.stage == BUFFER && strcmp(key, "events") == 0) {
+                s->nested_state.stage = BUFFER_EVENTS;
+                s->value_state = ACCEPT_VALUE;
             } else {
-                fprintf(stderr, "Unknown key: %s\n", s->key);
-                s->stage = ERROR;
+                fprintf(stderr, "Unknown key: %s\n", key);
+                s->pipe_state = ERROR;
             }
             break;
         case YAML_MAPPING_START_EVENT:
             /* Transform options are started. */
-            if (s->stage != ACCEPT_TRANSFORM &&
-                s->stage != ACCEPT_TRANSFORM_BUFFER) {
+            if (s->pipe_state != ACCEPT_TRANSFORM && /* TODO: Do not check pipe_root states */
+                s->pipe_state != ACCEPT_OUTPUT &&
+                s->nested_state.stage != BUFFER) {
                     fprintf(stderr, "Unexpected mapping event while getting 'transform' params: %d\n",
                     event->type);
-                    s->stage = ERROR;
+                    s->pipe_state = ERROR;
                 }
             break;
         case YAML_MAPPING_END_EVENT:
-            /* Pileline options are finished. */
-            switch (s->stage) {
-            case ACCEPT_TRANSFORM_BUFFER:
-                s->stage = ACCEPT_TRANSFORM;
+            /* Nested options are finished. */
+            switch (s->nested_state.stage) {
+            case BUFFER:
+                s->nested_state.stage = STAGE_ROOT;
                 break;
-            case ACCEPT_TRANSFORM:
-                s->stage = ACCEPT_PIPELINE;
+            case STAGE_ROOT:
+                /* Finished options of stage. */
+                s->pipe_state = ACCEPT_PIPELINE;
                 break;
             default:
                 fprintf(stderr, "Unexpected mapping_end event while getting 'transform' params: %d\n",
                     event->type);
-                s->stage = ERROR;
+                s->pipe_state = ERROR;
                 break;
             }
             break;
         default:
             fprintf(stderr, "Unexpected event while getting key: %d\n",
                     event->type);
-            s->state = ERROR;
+            s->value_state = ERROR;
             break;
         }
     }
-    else if (s->state == ACCEPT_VALUE){
+    else if (s->value_state == ACCEPT_VALUE){
         if (event->type != YAML_SCALAR_EVENT) {
             fprintf(stderr, "Unexpected event while getting value: %d\n",
                     event->type);
-            s->state = ERROR;
+            s->value_state = ERROR;
         } else {
             char *value = (char*)event->data.scalar.value;
-            switch (s->stage)
+            switch (s->nested_state.stage)
             {
-            case ACCEPT_TRANSFORM_WORKERS:
-                s->data.transform.workers_count = atoi(s->value);
-                s->stage = ACCEPT_TRANSFORM;
+            case WORKERS:
+                //s->data.transform.workers_count = atoi(s->value);
+                config->workers_count = atoi(value);
+                s->nested_state.stage = STAGE_ROOT;
                 break;
-            case ACCEPT_TRANSFORM_BUFFER_EVENTS:
-                s->data.transform.buffer.events_count = atoi(s->value);
-                s->stage = ACCEPT_TRANSFORM_BUFFER;
+            case BUFFER_EVENTS:
+                config->buffer.events_count = atoi(value);
+                s->nested_state.stage = BUFFER;
                 break;
-            case ACCEPT_TRANSFORM_BUFFER_SIZE:
-                s->data.transform.buffer.size = atoi(s->value);
-                s->stage = ACCEPT_TRANSFORM_BUFFER;
+            case BUFFER_SIZE:
+                config->buffer.size = atoi(value);
+                s->nested_state.stage = BUFFER;
                 break;
             default:
                 break;
             }
-            s->state = ACCEPT_KEY;
+            s->value_state = ACCEPT_KEY;
         }
     }
 }
@@ -230,7 +216,7 @@ int setTransform(struct parser_state *s, yaml_event_t *event)
 int consume_event(struct parser_state *s, yaml_event_t *event)
 {
     s->accepted = 0;
-    switch (s->stage) {
+    switch (s->pipe_state) {
     case START:
     case FINISH_PIPELINE:
         switch (event->type) {
@@ -240,14 +226,14 @@ int consume_event(struct parser_state *s, yaml_event_t *event)
             /* Document opened. */
         case YAML_SEQUENCE_START_EVENT:
             /* List of pipelines started. */
-            s->state = WAIT_DIRECTIVE;
+            s->value_state = WAIT_DIRECTIVE;
             break;
         case YAML_MAPPING_START_EVENT:
-            s->stage = ACCEPT_PIPELINE;
+            s->pipe_state = ACCEPT_PIPELINE;
             /* TODO: allocate memory for new pipeline config. */
             break;
         case YAML_STREAM_END_EVENT:
-            s->stage = STOP;
+            s->pipe_state = STOP;
             break;
         default:
             break;
@@ -257,31 +243,63 @@ int consume_event(struct parser_state *s, yaml_event_t *event)
     case ACCEPT_PIPELINE_NAME:
     case ACCEPT_PIPELINE_TYPE:
         /* Pipeline config starts with key name. */
-        if (s->state == WAIT_DIRECTIVE)
-            s->state = ACCEPT_KEY;
+        if (s->value_state == WAIT_DIRECTIVE)
+            s->value_state = ACCEPT_KEY;
         setPipeline(s,event);
         break;
     case ACCEPT_TRANSFORM:
-    case ACCEPT_TRANSFORM_WORKERS:
-    case ACCEPT_TRANSFORM_BUFFER:
-    case ACCEPT_TRANSFORM_BUFFER_SIZE:
-    case ACCEPT_TRANSFORM_BUFFER_EVENTS:
         /* Transform config starts with key name. */
-        setTransform(s, event);
+        setTransform(s, event, &s->data.transform);
+        break;
+    case ACCEPT_OUTPUT:
+        /* Transform config starts with key name. */
+        setTransform(s, event, &s->data.output);
         break;
     case ERROR:
     case STOP:
         break;
     }
 
-    return (s->state == ERROR ? 0 : 1);
+    return (s->pipe_state == ERROR ? 0 : 1);
+}
+
+int print_config(pipe_config *cfg)
+{
+    if (!cfg){
+        printf("cfg is NULL\n");
+        return 1;
+    }
+    printf("pipe name: %s\n"
+           "pipe type: %s\n"
+           "transform:\n"
+           "  workers: %d\n"
+           "    buffer:\n"
+           "      size: %ld\n"
+           "      events: %ld\n"
+           "  output:\n"
+           "    workers: %d\n"
+           "    buffer:\n"
+           "      size: %ld\n"
+           "      events: %ld\n",
+           cfg->name, cfg->type,
+           cfg->transform.workers_count, cfg->transform.buffer.size, cfg->transform.buffer.events_count,
+           cfg->output.workers_count, cfg->output.buffer.size, cfg->output.buffer.events_count
+    );
+    return 0;
+}
+
+int free_value_state(struct parser_state *ps)
+{
+    free(ps->data.name);
+    free(ps->data.type);
+    memset(ps, 0, sizeof(struct parser_state));
 }
 
 int parse_config(char *config_path)
 {
     yaml_parser_t parser;
     yaml_event_t event;
-    struct parser_state state = {.stage=START, .state=WAIT_DIRECTIVE, .accepted=0, .error=0};
+    struct parser_state value_state = {.pipe_state=START, .value_state=WAIT_DIRECTIVE, .accepted=0, .error=0};
     pipe_config data[64];
     int i = 0;
 
@@ -295,18 +313,16 @@ int parse_config(char *config_path)
         if (!yaml_parser_parse(&parser, &event)) {
             goto error;
         }
-        if (!consume_event(&state, &event)) {
+        if (!consume_event(&value_state, &event)) {
             goto error;
         }
-        if (state.accepted && i < sizeof(data)/sizeof(*data)) {
-            data[i].name = state.data.name;
-            data[i].type = state.data.type;
-            printf("data[%d]={name=%s, count=%s}\n",
-                i, data[i].name, data[i].type);
+        if (value_state.accepted && i < sizeof(data)/sizeof(*data)) {
+            print_config(&value_state.data);
+            free_value_state(&value_state);
             i++;
         }
         yaml_event_delete(&event);
-    } while (state.stage != STOP);
+    } while (value_state.pipe_state != STOP);
 
     yaml_parser_delete(&parser);
     return 0;
